c4cb0b90946498ca15fa8ac6239318ba06749797
Soochaehwa <43947445+Soochaehwa@users.noreply.github.com>
2021-06-12 00:44:25 +0900

    설정 추가

---
+++ config/yosbr/config/slightguimodifications/cts.groovy
@@ -0,0 +1,14 @@
+mainMenu {
+    enabled = true 
+    label {
+        position {
+            x = 2
+            y { it - 20 }
+        }
+        text = literal("Experience²")
+        align = "left"
+        color = 0xFFFFFF
+        shadow = true
+        onClicked = url("https://github.com/laxotor/Experience2")
+    }
+}
